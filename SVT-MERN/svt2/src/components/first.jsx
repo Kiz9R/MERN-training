2c1d94a109e8bc20a4d881b93ededc58c4a35a0f
kiz9r <ritankarxkiz9r@gmail.com>
2022-07-13 17:59:29 +0530

    day 2

---
+++ SVT-MERN/svt2/src/components/first.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const First = () => {
+  return (
+    <>
+      {/* <div>
+        <h3>
+          Name: <span>Ritankar Debnath</span>
+        </h3>
+        <ul>
+          <li>
+            <h4>Educational Qualification</h4>
+
+            <ol>
+              <li>ICSE</li>
+              <li>ISC</li>
+              <li>B.TECH</li>
+            </ol>
+            <li>
+              <h4>SKILL</h4>
+
+              <ol type="i">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+              </ol>
+            </li>
+          </li>
+        </ul>
+        <h1>IMAGE</h1>
+
+        <img src="./logo192.png" alt="no img" height="300px" width="280px" />
+      </div> */}
+
+      {/* table */}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Sno.</th>
+            <th>Name</th>
+            <th>Father's Name</th>
+            <th>Mother's name</th>
+            <th>Education</th>
+            <th>Mobile No.</th>
+            <th>DoB</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Blank</td>
+            <td>Kiz9r</td>
+            <td>Retro</td>
+            <td>Btech</td>
+            <td>9087554778</td>
+            <td>09.06.2003</td>
+            <td>anything</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Blank</td>
+            <td>Kiz9r</td>
+            <td>Retro</td>
+            <td>Btech</td>
+            <td>9087554778</td>
+            <td>09.06.2003</td>
+            <td>anything</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Blank</td>
+            <td>Kiz9r</td>
+            <td>Retro</td>
+            <td>Btech</td>
+            <td>9087554778</td>
+            <td>09.06.2003</td>
+            <td>anything</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default First;
