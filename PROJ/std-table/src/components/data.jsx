599648fc718a81398f2f2cbf69f64507b80dcb77
kiz9r <ritankarxkiz9r@gmail.com>
2022-07-14 18:32:40 +0530

    day 2

---
+++ PROJ/std-table/src/components/data.jsx
@@ -0,0 +1,353 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <>
+      <table border="3" align="center" rules="all" width="80%">
+        {/* Start of head */}
+        <thead>
+          <tr>
+            <th>
+              <h3>Sno.</h3>
+            </th>
+            <th>
+              <h3>Name</h3>
+            </th>
+            <th>
+              <h3>Father's name</h3>
+            </th>
+            <th>
+              <h3>Mother's name</h3>
+            </th>
+            <th>
+              <h3>Education</h3>
+            </th>
+            <th>
+              <h3>Mobile No.</h3>
+            </th>
+            <th>
+              <h3>DOB</h3>
+            </th>
+            <th>
+              <h3>Status</h3>
+            </th>
+          </tr>
+        </thead>
+        {/* End of head */}
+        {/* Start of body */}
+        <tbody>
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>1.</p>
+            </td>
+            <td>
+              <p>Neelima</p>
+            </td>
+            <td>
+              <p>Ashish</p>
+            </td>
+            <td>
+              <p>Pratima</p>
+            </td>
+            <td>
+              <p>BTECH</p>
+            </td>
+            <td>
+              <p>907xxxxxxx</p>
+            </td>
+            <td>
+              <p>09.06.99</p>
+            </td>
+            <td>
+              <p>Active</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>2.</p>
+            </td>
+            <td>
+              <p>Rajesh</p>
+            </td>
+            <td>
+              <p>Surya</p>
+            </td>
+            <td>
+              <p>Abha</p>
+            </td>
+            <td>
+              <p>BCA</p>
+            </td>
+            <td>
+              <p>893xxxxxxx</p>
+            </td>
+            <td>
+              <p>09.08.01</p>
+            </td>
+            <td>
+              <p>Deceased</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>3.</p>
+            </td>
+            <td>
+              <p>Suman</p>
+            </td>
+            <td>
+              <p>Mahinder</p>
+            </td>
+            <td>
+              <p>Amita</p>
+            </td>
+            <td>
+              <p>MCA</p>
+            </td>
+            <td>
+              <p>301xxxxxxx</p>
+            </td>
+            <td>
+              <p>10.10.10</p>
+            </td>
+            <td>
+              <p>InActive</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>4.</p>
+            </td>
+            <td>
+              <p>Neelima</p>
+            </td>
+            <td>
+              <p>Ashish</p>
+            </td>
+            <td>
+              <p>Pratima</p>
+            </td>
+            <td>
+              <p>12TH</p>
+            </td>
+            <td>
+              <p>569xxxxxxx</p>
+            </td>
+            <td>
+              <p>10.09.08</p>
+            </td>
+            <td>
+              <p>InActive</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>4.</p>
+            </td>
+            <td>
+              <p>Ratnam</p>
+            </td>
+            <td>
+              <p>Kshitija</p>
+            </td>
+            <td>
+              <p>Nandita</p>
+            </td>
+            <td>
+              <p>BCA</p>
+            </td>
+            <td>
+              <p>820xxxxxxx</p>
+            </td>
+            <td>
+              <p>04.08.01</p>
+            </td>
+            <td>
+              <p>Active</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>5.</p>
+            </td>
+            <td>
+              <p>Puneet</p>
+            </td>
+            <td>
+              <p>Balaram</p>
+            </td>
+            <td>
+              <p>Abha</p>
+            </td>
+            <td>
+              <p>PHD</p>
+            </td>
+            <td>
+              <p>290xxxxxxx</p>
+            </td>
+            <td>
+              <p>24.01.89</p>
+            </td>
+            <td>
+              <p>Active</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>6.</p>
+            </td>
+            <td>
+              <p>Dharma</p>
+            </td>
+            <td>
+              <p>Nisha</p>
+            </td>
+            <td>
+              <p>Vasanti</p>
+            </td>
+            <td>
+              <p>BTECH</p>
+            </td>
+            <td>
+              <p>709xxxxxxx</p>
+            </td>
+            <td>
+              <p>10.12.98</p>
+            </td>
+            <td>
+              <p>Deceased</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>7.</p>
+            </td>
+            <td>
+              <p>Kavitha</p>
+            </td>
+            <td>
+              <p>Sunil</p>
+            </td>
+            <td>
+              <p>Shanti</p>
+            </td>
+            <td>
+              <p>B.ED</p>
+            </td>
+            <td>
+              <p>079xxxxxxx</p>
+            </td>
+            <td>
+              <p>09.06.99</p>
+            </td>
+            <td>
+              <p>InActive</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>8.</p>
+            </td>
+            <td>
+              <p>Khushi</p>
+            </td>
+            <td>
+              <p>Uma</p>
+            </td>
+            <td>
+              <p>Rakhi</p>
+            </td>
+            <td>
+              <p>MTECH</p>
+            </td>
+            <td>
+              <p>890xxxxxxx</p>
+            </td>
+            <td>
+              <p>25.03.90</p>
+            </td>
+            <td>
+              <p>Active</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>9.</p>
+            </td>
+            <td>
+              <p>Aseem</p>
+            </td>
+            <td>
+              <p>Ramakanta</p>
+            </td>
+            <td>
+              <p>Geevarghese</p>
+            </td>
+            <td>
+              <p>NONE</p>
+            </td>
+            <td>
+              <p>567xxxxxxx</p>
+            </td>
+            <td>
+              <p>12.01.22</p>
+            </td>
+            <td>
+              <p>InActive</p>
+            </td>
+          </tr>
+          {/* Student end */}
+          <tr>
+            {/* Student start */}
+            <td>
+              <p>10.</p>
+            </td>
+            <td>
+              <p>Kalidas</p>
+            </td>
+            <td>
+              <p>Pitambar</p>
+            </td>
+            <td>
+              <p>Lata</p>
+            </td>
+            <td>
+              <p>BTECH</p>
+            </td>
+            <td>
+              <p>802xxxxxxx</p>
+            </td>
+            <td>
+              <p>09.06.99</p>
+            </td>
+            <td>
+              <p>Deceased</p>
+            </td>
+          </tr>
+          {/* Student end */}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
