e251af4207b20ccbbf82b158c5298e3075f676c5
kiz9r <ritankarxkiz9r@gmail.com>
2022-07-20 12:00:31 +0530

    day 4

---
+++ SVT-MERN/svt4/src/components/first.jsx
@@ -0,0 +1,63 @@
+const Pattern = () => {
+  return (
+    <main>
+      <div className="first"> </div>
+
+      <div className="second" style={{ backgroundColor: "#DE834D" }}>
+        {" "}
+      </div>
+      <div className="second" style={{ backgroundColor: "#FF4949" }}>
+        {" "}
+      </div>
+      <div className="second" style={{ backgroundColor: "#DE834D" }}>
+        {" "}
+      </div>
+      <div className="second" style={{ backgroundColor: "#54BAB9" }}>
+        {" "}
+      </div>
+      <div className="second" style={{ backgroundColor: "#EF5B0C" }}>
+        {" "}
+      </div>
+
+      <div className="third"> </div>
+      <div className="fourth"> </div>
+      <div className="fourth" style={{ backgroundColor: "white" }}>
+        {" "}
+      </div>
+      <div className="fourth" style={{ backgroundColor: "#7F5283" }}>
+        {" "}
+      </div>
+      <div className="fourth" style={{ backgroundColor: "#A6D1E6" }}>
+        {" "}
+      </div>
+      <div className="fourth" style={{ backgroundColor: "#FFE5B4" }}>
+        {" "}
+      </div>
+
+      <div className="fifth" style={{ backgroundColor: "#810955" }}>
+        {" "}
+      </div>
+      <div className="sixth" style={{ backgroundColor: "#F4E06D" }}>
+        {" "}
+      </div>
+      <div className="seventh" style={{ backgroundColor: "#D4D925" }}>
+        {" "}
+      </div>
+
+      <div className="eighth" style={{ backgroundColor: "#E26A2C" }}>
+        {" "}
+      </div>
+      <div className="eighth" style={{ backgroundColor: "#CA4E79" }}>
+        {" "}
+      </div>
+      <div className="eighth" style={{ backgroundColor: "#E8A0BF" }}>
+        {" "}
+      </div>
+      <div className="ninth" style={{ backgroundColor: "#3A5BA0" }}>
+        {" "}
+      </div>
+    </main>
+  );
+};
+
+export default Pattern;
