8c74cfb2c2509c2d336ff5a06ae0da65e4e84fae
kiz9r <ritankarxkiz9r@gmail.com>
2022-07-21 22:39:27 +0530

    day 5

--- PROJ/website-1/src/components/staf.jsx
+++ PROJ/website-1/src/components/staf.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import people from "../people";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Staf = () => {
   return (
@@ -14,22 +15,22 @@ const Staf = () => {
         </div>
         <div className="section center featured-center">
           {/* one card */}
-          <div className="tour-card">
-            <div className="tour-img-container">
+          <div className="staf-card">
+            <div className="staf-img-container">
               <img
                 src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg"
                 alt="person"
-                className="tour-img"
+                className="staf-img"
               />
             </div>
-            <div className="tour-footer">
-              <h4 className="tour-title">maria ferguson</h4>
-              <div className="tour-info">
-                <p className="tour-country">
+            <div className="staf-footer">
+              <h4 className="staf-title">maria ferguson</h4>
+              <div className="staf-info">
+                <p className="staf-country">
                   <FaMapMarkerAlt />
                   United States
                 </p>
-                <div className="tour-details">
+                <div className="staf-details">
                   <p>
                     DEPT : <span className="dept">cse</span>{" "}
                   </p>
@@ -39,24 +40,24 @@ const Staf = () => {
           </div>
           {/* one card */}
           {/* one card */}
-          <div className="tour-card">
-            <div className="tour-img-container">
+          <div className="staf-card">
+            <div className="staf-img-container">
               <img
                 src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg"
                 alt="person"
-                className="tour-img"
+                className="staf-img"
               />
             </div>
-            <div className="tour-footer">
-              <h4 className="tour-title">peter smith</h4>
-              <div className="tour-info">
-                <p className="tour-country">
+            <div className="staf-footer">
+              <h4 className="staf-title">peter smith</h4>
+              <div className="staf-info">
+                <p className="staf-country">
                   <FaMapMarkerAlt />
-                  United States
+                  Nigeria
                 </p>
-                <div className="tour-details">
+                <div className="staf-details">
                   <p>
-                    DEPT : <span className="dept">cse</span>{" "}
+                    DEPT : <span className="dept">ee</span>{" "}
                   </p>
                 </div>
               </div>
@@ -64,22 +65,22 @@ const Staf = () => {
           </div>
           {/* one card */}
           {/* one card */}
-          <div className="tour-card">
-            <div className="tour-img-container">
+          <div className="staf-card">
+            <div className="staf-img-container">
               <img
                 src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
                 alt="person"
-                className="tour-img"
+                className="staf-img"
               />
             </div>
-            <div className="tour-footer">
-              <h4 className="tour-title">john doe</h4>
-              <div className="tour-info">
-                <p className="tour-country">
+            <div className="staf-footer">
+              <h4 className="staf-title">john doe</h4>
+              <div className="staf-info">
+                <p className="staf-country">
                   <FaMapMarkerAlt />
                   Canada
                 </p>
-                <div className="tour-details">
+                <div className="staf-details">
                   <p>
                     DEPT : <span className="dept">ece</span>{" "}
                   </p>
@@ -88,6 +89,11 @@ const Staf = () => {
             </div>
           </div>
           {/* one card */}
+        </div>
+        <div className="staf-link">
+          <NavLink to="/" className="main-btn">
+            Explore more
+          </NavLink>
         </div>
       </section>
     </>
