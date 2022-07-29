d111fd778719fb31052837b2d3574a6b099d0427
kiz9r <ritankarxkiz9r@gmail.com>
2022-07-29 16:57:09 +0530

    day 7

--- SVT-MERN/svt5/src/App.js
+++ SVT-MERN/svt5/src/App.js
@@ -3,19 +3,83 @@ import "./App.css";
 // import First from "./components/first";
 import Second from "./components/second";
 
+// const name = "Hi! My name is Ritankar Debnath";
+
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route extact path="/" element={<First />}>
-            {" "}
-          </Route>
-        </Routes>
-      </BrowserRouter> */}
-
-      {/* <First /> */}
-      <Second />
+      <div className="App">
+        <div className="title">
+          <h1 className="text-center">Netfilx</h1>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Second
+                name="Netflix"
+                des="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                img="img/1.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- SVT-MERN/svt5/src/components/second.jsx
+++ SVT-MERN/svt5/src/components/second.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-const Second = () => {
+const Second = (props) => {
   return (
     <>
-      <h1>second</h1>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-11">
+            <div className="row">
+              <div className="col-12">
+                <div className="card1">
+                  <img
+                    src={props.img}
+                    height="150px"
+                    width="100%"
+                    alt="no img"
+                  />
+                  <h3>{props.name}</h3>
+                  <span>{props.des}</span>
+                  <a href="">
+                    <button>watch now</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
